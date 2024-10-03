a7a26e9b289c3a7af90b515bf739c18fbe01d666
badgiro <mulbashev98@gmail.com>
2024-10-04 01:25:24 +0300

    test

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,4 +1,4 @@
-import styles from "./App.module.css";
+
 import PeoplePage from "../PeoplePage/PeoplePage";
 
 
--- src/containers/PeoplePage/PeoplePage.jsx
+++ src/containers/PeoplePage/PeoplePage.jsx
@@ -1,33 +1,44 @@
-import { useEffect, useState } from "react";
-import { getApiResource } from "../../utils/network";
-import { PEOPLE_API } from "../../constants/api";
-import { getPeopleId, getPeopleImg } from "../../services/getPeopleData";
-import PeopleList from "../../components/PeoplePage/PeopleList/PeopleList";
+import { useEffect, useState } from 'react'
+import { getApiResource } from '../../utils/network'
+import { PEOPLE_API } from '../../constants/api'
+import { getPeopleId, getPeopleImg } from '../../services/getPeopleData'
+import PeopleList from '../../components/PeoplePage/PeopleList/PeopleList'
 
 const PeoplePage = () => {
-  const [people, setPeople] = useState(null);
+  const [people, setPeople] = useState(null)
+  const [errorApi, setErrorApi] = useState(false)
   const getResource = async (url) => {
-    const res = await getApiResource(url);
-    const peopleList = res.results.map(({ name, url }) => {
-      const id = getPeopleId(url);
-      const img = getPeopleImg(id);
+    const res = await getApiResource(url)
+    if (res) {
+      const peopleList = res.results.map(({ name, url }) => {
+        const id = getPeopleId(url)
+        const img = getPeopleImg(id)
 
-      return {
-        img,
-        name,
-        id,
-      };
-    });
-    setPeople(peopleList);
-  };
+        return {
+          img,
+          name,
+
+          id,
+        }
+      })
+      setPeople(peopleList)
+      setErrorApi(false)
+    } else {
+      setErrorApi(true)
+    }
+  }
   useEffect(() => {
-    getResource(PEOPLE_API);
-  }, []);
+    getResource(PEOPLE_API)
+  }, [])
   return (
     <>
-      {people && <PeopleList people={people} />}
+      {errorApi ? (
+        <h1>Error</h1>
+      ) : (
+        <>{people && <PeopleList people={people} />}</>
+      )}
     </>
-  );
-};
+  )
+}
 
-export default PeoplePage;
+export default PeoplePage
